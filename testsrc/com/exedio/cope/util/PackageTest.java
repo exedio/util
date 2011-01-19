22d32cec2d4bf84247bf5b1b47161d34d164d56a
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2011-01-19 10:42:27 +0000

    add SystemPropertySourceTest
    git-svn-id: https://svn.exedio.com/cope/util/trunk@18792 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- testsrc/com/exedio/cope/util/PackageTest.java
+++ testsrc/com/exedio/cope/util/PackageTest.java
@@ -40,6 +40,7 @@ public class PackageTest extends TestCase
 		suite.addTestSuite(PoolTest.class);
 		suite.addTestSuite(PoolCounterTest.class);
 		suite.addTestSuite(PropertiesTest.class);
+		suite.addTestSuite(SystemPropertySourceTest.class);
 		suite.addTestSuite(PrefixSourceTest.class);
 		suite.addTestSuite(SequenceCheckerTest.class);
 		suite.addTestSuite(XMLEncoderTest.class);
