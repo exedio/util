eccea2878b05e425bdd56c5bb8031f8b435cf500
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2012-09-21 19:26:07 +0000

    test what will be deprecated soon
    git-svn-id: https://svn.exedio.com/cope/util/trunk@24385 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ testsrc/com/exedio/cope/util/PropertiesDeprecatedTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2004-2009  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.util;
+
+import java.util.Arrays;
+
+import com.exedio.cope.junit.CopeAssert;
+
+@edu.umd.cs.findbugs.annotations.SuppressWarnings("SIC_INNER_SHOULD_BE_STATIC_ANON") // is more compact to write in tests
+public class PropertiesDeprecatedTest extends CopeAssert
+{
+	static class TestProperties extends Properties
+	{
+		final BooleanField boolFalse = new BooleanField("boolFalse", false);
+		final BooleanField boolTrue = new BooleanField("boolTrue", true);
+		final IntField int10 = new IntField("int10", 10, 5);
+		final StringField stringMandatory = new StringField("stringMandatory");
+		final StringField stringOptional = new StringField("stringOptional", "stringOptional.defaultValue");
+		final StringField stringHidden = new StringField("stringHidden", true);
+		final FileField file = new FileField("file");
+		final MapField map = new MapField("map");
+
+		TestProperties(final java.util.Properties source, final String sourceDescription)
+		{
+			super(getSource(source, sourceDescription), null);
+		}
+
+		void assertIt()
+		{
+			assertEquals(list(), getTests());
+			assertEqualsUnmodifiable(Arrays.asList(new Properties.Field[]{
+					boolFalse,
+					boolTrue,
+					int10,
+					stringMandatory,
+					stringOptional,
+					stringHidden,
+					file,
+					map,
+			}), getFields());
+
+			assertEquals("boolFalse", boolFalse.getKey());
+			assertEquals("boolTrue", boolTrue.getKey());
+			assertEquals("int10", int10.getKey());
+			assertEquals("stringMandatory", stringMandatory.getKey());
+			assertEquals("stringOptional", stringOptional.getKey());
+			assertEquals("stringHidden", stringHidden.getKey());
+			assertEquals("file", file.getKey());
+			assertEquals("map", map.getKey());
+
+			assertEquals(Boolean.FALSE, boolFalse.getDefaultValue());
+			assertEquals(Boolean.TRUE, boolTrue.getDefaultValue());
+			assertEquals(Integer.valueOf(10), int10.getDefaultValue());
+			assertEquals(null, stringMandatory.getDefaultValue());
+			assertEquals("stringOptional.defaultValue", stringOptional.getDefaultValue());
+			assertEquals(null, stringHidden.getDefaultValue());
+			assertEquals(null, file.getDefaultValue());
+			assertEquals(null, map.getDefaultValue());
+
+			assertEquals(false, boolFalse.hasHiddenValue());
+			assertEquals(false, boolTrue.hasHiddenValue());
+			assertEquals(false, int10.hasHiddenValue());
+			assertEquals(false, stringMandatory.hasHiddenValue());
+			assertEquals(false, stringOptional.hasHiddenValue());
+			assertEquals(true, stringHidden.hasHiddenValue());
+			assertEquals(false, file.hasHiddenValue());
+			assertEquals(false, map.hasHiddenValue());
+		}
+	}
+
+	public void testIt()
+	{
+		final java.util.Properties pminimal = new java.util.Properties();
+		pminimal.setProperty("stringMandatory", "stringMandatory.minimalValue");
+		pminimal.setProperty("stringHidden", "stringHidden.minimalValue");
+
+		final TestProperties minimal = new TestProperties(pminimal, "minimal");
+		minimal.assertIt();
+	}
+}
