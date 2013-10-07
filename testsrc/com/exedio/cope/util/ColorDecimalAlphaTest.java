b431f83160fc85f789c46d38410b7c72153106a6
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2013-10-07 09:31:33 +0000

    test more with rounding
    git-svn-id: https://svn.exedio.com/cope/util/trunk@26668 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- testsrc/com/exedio/cope/util/ColorDecimalAlphaTest.java
+++ testsrc/com/exedio/cope/util/ColorDecimalAlphaTest.java
@@ -18,7 +18,10 @@
 
 package com.exedio.cope.util;
 
+import static junit.framework.Assert.assertEquals;
+
 import com.exedio.cope.junit.CopeAssert;
+import java.text.DecimalFormat;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -30,8 +33,15 @@ public class ColorDecimalAlphaTest extends CopeAssert
 	{
 		for(int alphaPercent = 0; alphaPercent<=100; alphaPercent++)
 		{
+			final String expectedDecimal = "" + (alphaPercent/100) + ',' + ((alphaPercent/10)%10) + ((alphaPercent)%10);
 			final int alphaByte = (int)(alphaPercent * 2.55f);
-			System.out.println("" + alphaPercent + " " + alphaByte);
+			@SuppressWarnings("cast")
+			final float alphaFloat = ((float)alphaByte)/255f;
+			final int alphaByteFromFloat = (((int)(alphaFloat*255)) & 0xFF);
+			assertEquals(alphaByte, alphaByteFromFloat);
+			final String actualDecimal = new DecimalFormat("#########0.00").format(alphaFloat);
+			assertEquals(expectedDecimal, actualDecimal);
+			System.out.println("" + alphaPercent + " " + alphaByte + "  " + alphaByteFromFloat + "  " + expectedDecimal + "  " + actualDecimal + "  " + alphaFloat);
 		}
 	}
 }
