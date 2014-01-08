5dca622328d947c0b14782fdb225ecda83fb1075
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2014-01-08 10:58:18 +0000

    Pool will not fail anymore, if Factory#isValidOnPut returns false (fix javadoc)
    git-svn-id: https://svn.exedio.com/cope/util/trunk@27399 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- src/com/exedio/cope/util/Pool.java
+++ src/com/exedio/cope/util/Pool.java
@@ -33,7 +33,7 @@ public final class Pool<E>
 		/**
 		 * If this method returns false, it should also try its best
 		 * to release any resources associated with <tt>e</tt>,
-		 * because in this case {@link #dispose()} is not called by the pool.
+		 * because in this case {@link #dispose(Object)} is not called by the pool.
 		 */
 		boolean isValidOnPut(E e);
 		void dispose(E e);
