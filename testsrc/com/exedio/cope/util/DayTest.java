2940c25ef0a19a85bc1ad8d99cbff551d197e873
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2014-03-06 16:39:08 +0000

    reuse date
    git-svn-id: https://svn.exedio.com/cope/util/trunk@27935 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- testsrc/com/exedio/cope/util/DayTest.java
+++ testsrc/com/exedio/cope/util/DayTest.java
@@ -150,11 +150,13 @@ public class DayTest extends CopeAssert
 		final SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS (Z)");
 
 		final Day summer = new Day(2005, 9, 23);
+		final Day winter = new Day(2005, 2, 22);
+
 		assertEquals(df.parse("2005-09-23 00:00:00.000 (+0200)"), summer.getTimeFrom());
 		assertEquals(df.parse("2005-09-23 23:59:59.999 (+0200)"), summer.getTimeTo());
 
-		assertEquals(new Day(2005, 2, 22), valueOf(df.parse("2005-02-22 00:00:00.000 (+0100)")));
-		assertEquals(new Day(2005, 2, 22), valueOf(df.parse("2005-02-22 23:59:59.999 (+0100)")));
+		assertEquals(winter, valueOf(df.parse("2005-02-22 00:00:00.000 (+0100)")));
+		assertEquals(winter, valueOf(df.parse("2005-02-22 23:59:59.999 (+0100)")));
 	}
 	@Test
 	public final void conversionMillis() throws ParseException
