7b6026300412b63d212afa82abe7780314b3427d
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2017-02-23 18:27:26 +0100

    idea: inspection junit4 with junit3 super class

--- testsrc/com/exedio/cope/util/XMLEncoderTest.java
+++ testsrc/com/exedio/cope/util/XMLEncoderTest.java
@@ -18,10 +18,12 @@
 
 package com.exedio.cope.util;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
-public class XMLEncoderTest extends TestCase
+public class XMLEncoderTest
 {
 	private static void assertIt(final String expected, final String actual)
 	{
