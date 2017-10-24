a77915d26e1b1f5686c777d6bb2268ceccdb8ee1
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2017-10-24 08:53:57 +0200

    ProxyJobContextTest amended

--- testsrc/com/exedio/cope/util/ProxyJobContextTest.java
+++ testsrc/com/exedio/cope/util/ProxyJobContextTest.java
@@ -19,28 +19,162 @@
 package com.exedio.cope.util;
 
 import static com.exedio.cope.util.JobContextDeprecated.requestedToStop;
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.Test;
 
 public class ProxyJobContextTest
 {
-	@Test public void testIt()
+	private JC target;
+	private ProxyJobContext c;
+	@BeforeEach void before()
 	{
-		final ProxyJobContext c = new ProxyJobContext(new EmptyJobContext());
-
-		c.stopIfRequested();
-		assertEquals(false, requestedToStop(c));
-		assertEquals(false, c.supportsMessage());
-		assertEquals(false, c.supportsProgress());
-		assertEquals(false, c.supportsCompleteness());
-
-		c.setMessage("");
-		c.incrementProgress();
-		c.incrementProgress(5);
-		c.setCompleteness(0.5);
+		target = new JC();
+		c = new ProxyJobContext(target);
 	}
+
+	@Test void testStopIfRequested()
+	{
+		c.stopIfRequested();
+		target.assertIt("stopIfRequested");
+	}
+	@Test void testRequestedToStop()
+	{
+		assertEquals(false, requestedToStop(c));
+		target.assertIt("requestedToStop");
+
+		target.requestedToStop = true;
+		assertEquals(true, requestedToStop(c));
+		target.assertIt("requestedToStop");
+	}
+	@Test void testSupportsMessage()
+	{
+		assertEquals(false, c.supportsMessage());
+		target.assertIt("supportsMessage");
+
+		target.supportsMessage = true;
+		assertEquals(true, c.supportsMessage());
+		target.assertIt("supportsMessage");
+	}
+	@Test void testSupportsProgress()
+	{
+		assertEquals(false, c.supportsProgress());
+		target.assertIt("supportsProgress");
+
+		target.supportsProgress = true;
+		assertEquals(true, c.supportsProgress());
+		target.assertIt("supportsProgress");
+	}
+	@Test void testSupportsCompleteness()
+	{
+		assertEquals(false, c.supportsCompleteness());
+		target.assertIt("supportsCompleteness");
+
+		target.supportsCompleteness = true;
+		assertEquals(true, c.supportsCompleteness());
+		target.assertIt("supportsCompleteness");
+	}
+	@Test void testSetMessage()
+	{
+		c.setMessage("");
+		target.assertIt("setMessage()");
+
+		c.setMessage("myMessage");
+		target.assertIt("setMessage(myMessage)");
+	}
+	@Test void testIncrementProgress()
+	{
+		c.incrementProgress();
+		target.assertIt("incrementProgress");
+
+		c.incrementProgress(5);
+		target.assertIt("incrementProgress(5)");
+	}
+	@Test void testSetCompleteness()
+	{
+		c.setCompleteness(0.5);
+		target.assertIt("setCompleteness(0.5)");
+	}
+
+	private static final class JC extends AssertionErrorJobContext
+	{
+		private final ArrayList<String> actual = new ArrayList<>();
+
+		void assertIt(final String... expected)
+		{
+			assertEquals(asList(expected), actual);
+			actual.clear();
+		}
+
+		@Override
+		public void stopIfRequested()
+		{
+			actual.add("stopIfRequested");
+		}
+
+		boolean requestedToStop = false;
+		@Override
+		@Deprecated
+		@SuppressWarnings("deprecation") // needed for idea
+		public boolean requestedToStop()
+		{
+			actual.add("requestedToStop");
+			return requestedToStop;
+		}
+
+		boolean supportsMessage = false;
+		@Override
+		public boolean supportsMessage()
+		{
+			actual.add("supportsMessage");
+			return supportsMessage;
+		}
+
+		@Override
+		public void setMessage(final String message)
+		{
+			actual.add("setMessage(" + message + ")");
+		}
+
+		boolean supportsProgress = false;
+		@Override
+		public boolean supportsProgress()
+		{
+			actual.add("supportsProgress");
+			return supportsProgress;
+		}
+
+		@Override
+		public void incrementProgress()
+		{
+			actual.add("incrementProgress");
+		}
+
+		@Override
+		public void incrementProgress(final int delta)
+		{
+			actual.add("incrementProgress(" + delta + ")");
+		}
+
+		boolean supportsCompleteness = false;
+		@Override
+		public boolean supportsCompleteness()
+		{
+			actual.add("supportsCompleteness");
+			return supportsCompleteness;
+		}
+
+		@Override
+		public void setCompleteness(final double completeness)
+		{
+			actual.add("setCompleteness(" + completeness + ")");
+		}
+	}
+
 
 	@SuppressWarnings("unused")
 	@Test public void testNull()
