a1bcfc337aec486a440e9668456854785bce76c6
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2018-11-08 11:16:13 +0100

    Check copied from cope
    663984514ee3b8b471f248267f34e74c6b07ed01
    Thu Nov 8 15:27:09 2018 +0100
    CheckTest uses assertFails

---
+++ src/com/exedio/cope/util/Check.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2004-2015  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.util;
+
+import java.lang.reflect.Array;
+
+public final class Check
+{
+	public static int requireGreaterZero(final int value, final String name)
+	{
+		if(value<=0)
+			throw new IllegalArgumentException(name + " must be greater zero, but was " + value);
+		return value;
+	}
+
+	public static long requireGreaterZero(final long value, final String name)
+	{
+		if(value<=0)
+			throw new IllegalArgumentException(name + " must be greater zero, but was " + value);
+		return value;
+	}
+
+	public static int requireNonNegative(final int value, final String name)
+	{
+		if(value<0)
+			throw new IllegalArgumentException(name + " must not be negative, but was " + value);
+		return value;
+	}
+
+	public static long requireNonNegative(final long value, final String name)
+	{
+		if(value<0)
+			throw new IllegalArgumentException(name + " must not be negative, but was " + value);
+		return value;
+	}
+
+	public static String requireNonEmpty(final String value, final String name)
+	{
+		if(value==null)
+			throw new NullPointerException(name);
+		if(value.isEmpty())
+			throw new IllegalArgumentException(name + " must not be empty");
+		return value;
+	}
+
+	/**
+	 * Besides checking the value, this method returns a copy of the given value
+	 * to avoid later modifications of the value by the caller.
+	 */
+	public static <T> T[] requireNonEmptyAndCopy(final T[] value, final String name)
+	{
+		if(value==null)
+			throw new NullPointerException(name);
+		if(value.length==0)
+			throw new IllegalArgumentException(name + " must not be empty");
+
+		@SuppressWarnings("unchecked")
+		final T[] result = (T[])Array.newInstance(value.getClass().getComponentType(), value.length);
+		for(int i = 0; i<value.length; i++)
+		{
+			final T s = value[i];
+			if(s==null)
+				throw new NullPointerException(name + '[' + i + ']');
+			result[i] = s;
+		}
+
+		return result;
+	}
+
+	/**
+	 * Besides checking the value, this method returns a copy of the given value
+	 * to avoid later modifications of the value by the caller.
+	 */
+	public static String[] requireNonEmptyAndCopy(final String[] value, final String name)
+	{
+		if(value==null)
+			throw new NullPointerException(name);
+		if(value.length==0)
+			throw new IllegalArgumentException(name + " must not be empty");
+
+		final String[] result = new String[value.length];
+		for(int i = 0; i<value.length; i++)
+		{
+			final String s = value[i];
+			if(s==null)
+				throw new NullPointerException(name + '[' + i + ']');
+			if(s.isEmpty())
+				throw new IllegalArgumentException(name + '[' + i + "] must not be empty");
+			result[i] = s;
+		}
+
+		return result;
+	}
+
+	private Check()
+	{
+		// prevent instantiation
+	}
+}
---
+++ testsrc/com/exedio/cope/util/CheckTest.java
@@ -0,0 +1,193 @@
+/*
+ * Copyright (C) 2004-2015  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.util;
+
+import static com.exedio.cope.junit.Assert.assertFails;
+import static com.exedio.cope.util.Check.requireGreaterZero;
+import static com.exedio.cope.util.Check.requireNonEmpty;
+import static com.exedio.cope.util.Check.requireNonEmptyAndCopy;
+import static com.exedio.cope.util.Check.requireNonNegative;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings({"ConstantConditions", "ResultOfMethodCallIgnored"})
+@SuppressFBWarnings("NP_NULL_PARAM_DEREF_NONVIRTUAL")
+public class CheckTest
+{
+	@Test void testRequireGreaterZeroInt()
+	{
+		assertEquals(1, requireGreaterZero(1, "name"));
+	}
+	@Test void testRequireGreaterZeroIntZero()
+	{
+		assertFails(() ->
+			requireGreaterZero(0, "name"),
+			IllegalArgumentException.class,
+			"name must be greater zero, but was 0");
+	}
+	@Test void testRequireGreaterZeroIntNegative()
+	{
+		assertFails(() ->
+			requireGreaterZero(-1, "name"),
+			IllegalArgumentException.class,
+			"name must be greater zero, but was -1");
+	}
+
+	@Test void testRequireGreaterZeroLong()
+	{
+		assertEquals(1, requireGreaterZero(1l, "name"));
+	}
+	@Test void testRequireGreaterZeroLongZero()
+	{
+		assertFails(() ->
+			requireGreaterZero(0l, "name"),
+			IllegalArgumentException.class,
+			"name must be greater zero, but was 0");
+	}
+	@Test void testRequireGreaterZeroLongNegative()
+	{
+		assertFails(() ->
+			requireGreaterZero(-1l, "name"),
+			IllegalArgumentException.class,
+			"name must be greater zero, but was -1");
+	}
+
+	@Test void testRequireNonNegativeInt()
+	{
+		assertEquals(1, requireNonNegative(1, "name"));
+	}
+	@Test void testRequireNonNegativeIntZero()
+	{
+		assertEquals(0, requireNonNegative(0, "name"));
+	}
+	@Test void testRequireNonNegativeIntNegative()
+	{
+		assertFails(() ->
+			requireNonNegative(-1, "name"),
+			IllegalArgumentException.class,
+			"name must not be negative, but was -1");
+	}
+
+	@Test void testRequireNonNegativeLong()
+	{
+		assertEquals(1l, requireNonNegative(1l, "name"));
+	}
+	@Test void testRequireNonNegativeLongZero()
+	{
+		assertEquals(0l, requireNonNegative(0l, "name"));
+	}
+	@Test void testRequireNonNegativeLongNegative()
+	{
+		assertFails(() ->
+			requireNonNegative(-1l, "name"),
+			IllegalArgumentException.class,
+			"name must not be negative, but was -1");
+	}
+
+	@SuppressFBWarnings("ES_COMPARING_STRINGS_WITH_EQ")
+	@Test void testRequireNonEmptyString()
+	{
+		assertSame("x", requireNonEmpty("x", "name"));
+	}
+	@Test void testRequireNonEmptyStringNull()
+	{
+		assertFails(() ->
+			requireNonEmpty(null, "name"),
+			NullPointerException.class,
+			"name");
+	}
+	@Test void testRequireNonEmptyStringEmpty()
+	{
+		assertFails(() ->
+			requireNonEmpty("", "name"),
+			IllegalArgumentException.class,
+			"name must not be empty");
+	}
+
+	@Test void testRequireNonEmptyAndCopyObjectsCopy()
+	{
+		final Object[] original = {"a", "b", "c"};
+		final Object[] copy = requireNonEmptyAndCopy(original, "name");
+		assertEquals(Arrays.asList("a", "b", "c"), Arrays.asList(copy));
+		assertNotSame(original, copy);
+	}
+	@Test void testRequireNonEmptyAndCopyObjectsNull()
+	{
+		assertFails(() ->
+			requireNonEmptyAndCopy((Object[])null, "name"),
+			NullPointerException.class,
+			"name");
+	}
+	@Test void testRequireNonEmptyAndCopyObjectsEmpty()
+	{
+		assertFails(() ->
+			requireNonEmptyAndCopy(new Object[0], "name"),
+			IllegalArgumentException.class,
+			"name must not be empty");
+	}
+	@Test void testRequireNonEmptyAndCopyObjectsElementNull()
+	{
+		assertFails(() ->
+			requireNonEmptyAndCopy(new Object[]{"hallo", null}, "name"),
+			NullPointerException.class,
+			"name[1]");
+	}
+
+	@Test void testRequireNonEmptyAndCopyStringsCopy()
+	{
+		final String[] original = {"a", "b", "c"};
+		final String[] copy = requireNonEmptyAndCopy(original, "name");
+		assertEquals(Arrays.asList("a", "b", "c"), Arrays.asList(copy));
+		assertNotSame(original, copy);
+	}
+	@SuppressWarnings("RedundantCast")
+	@Test void testRequireNonEmptyAndCopyStringsNull()
+	{
+		assertFails(() ->
+			requireNonEmptyAndCopy((String[])null, "name"),
+			NullPointerException.class,
+			"name");
+	}
+	@Test void testRequireNonEmptyAndCopyStringsEmpty()
+	{
+		assertFails(() ->
+			requireNonEmptyAndCopy(new String[0], "name"),
+			IllegalArgumentException.class,
+			"name must not be empty");
+	}
+	@Test void testRequireNonEmptyAndCopyStringsElementNull()
+	{
+		assertFails(() ->
+			requireNonEmptyAndCopy(new String[]{"hallo", null}, "name"),
+			NullPointerException.class,
+			"name[1]");
+	}
+	@Test void testRequireNonEmptyAndCopyStringsElementEmpty()
+	{
+		assertFails(() ->
+			requireNonEmptyAndCopy(new String[]{"hallo", ""}, "name"),
+			IllegalArgumentException.class,
+			"name[1] must not be empty");
+	}
+}
